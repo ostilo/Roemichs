0e21f92434298e149e50197fc3737a356368cc16
OLALEKAN TAIYE AYODEJI <olalekanayodeji79@gmail.com@gmail.com>
2020-04-01 13:24:08 +0100

    Second Projection

--- app/src/main/java/com/elkanah/roemichs/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/elkanah/roemichs/ui/fragments/LoginFragment.java
@@ -3,10 +3,12 @@ package com.elkanah.roemichs.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.elkanah.roemichs.R;
 
@@ -14,7 +16,7 @@ import com.elkanah.roemichs.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
     public LoginFragment() {
         // Required empty public constructor
@@ -25,6 +27,15 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        Button btnLogin = v.findViewById(R.id.button);
+        btnLogin.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_parentFragment);
     }
 }
